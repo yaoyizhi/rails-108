217bfc25c5d6fd9d38026b5af457cfdd8f0fdd53
yaoyizhi <email@yaoadou888>
2017-03-25 08:33:36 +0800

    implement new action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,31 @@
 class GroupsController < ApplicationController
+    def index
+        @groups = Group.all
+    end
+
+    def new
+        @group = Group.new
+    end
+
+    def create
+        @group = Group.new(group_params)
+        @group.save
+        redirect_to groups_path
+    end
+
+    def edit
+        @group = Group.find(params[:id])
+    end
+
+    def update
+        @group = Group.find(params[:id])
+        @group.update(group_params)
+        redirect_to groups_path
+    end
+
+    private
+
+    def group_params
+        params.require(:group).permit(:title, :description)
+    end
 end
