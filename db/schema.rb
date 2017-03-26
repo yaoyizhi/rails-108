49a5c88ebb15f3157e7aa7757293ea388627de6c
yaoyizhi <email@yaoadou888>
2017-03-27 04:30:52 +0800

    implement creating post action

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170325213519) do
+ActiveRecord::Schema.define(version: 20170325215004) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170325213519) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "content"
   end
 
   create_table "users", force: :cascade do |t|
